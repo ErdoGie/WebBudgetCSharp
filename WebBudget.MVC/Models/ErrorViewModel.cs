23cb81465aab878eb9d6aa779debe3e256ee35fe
RdoG <radoslaw.gucwa.programista@gmail.com>
2023-08-06 15:05:57 +0200

    Initialized_first_commit

---
+++ WebBudget.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebBudget.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
