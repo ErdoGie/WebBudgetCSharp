3d8233dbcf03019e4eb0b678a4382080e797bdf0
RdoG <radoslaw.gucwa.programista@gmail.com>
2023-08-15 11:13:10 +0200

    Checked_after_conflicts_if_program_works_and_pushing_to_GH

--- WebBudget.Domain/Interfaces/IWebBudgetRepository.cs
+++ WebBudget.Domain/Interfaces/IWebBudgetRepository.cs
@@ -1,25 +1,25 @@
 ﻿namespace WebBudget.Domain.Interfaces
 {
-	public interface IWebBudgetRepository
-	{
+    public interface IWebBudgetRepository
+    {
 
-		Task CreateIncome(Domain.Entities.WebBudgetIncome webBudgetIncome);
-		Task CreateExpense(Domain.Entities.WebBudgetExpense webBudgetExpense);
+        Task CreateIncome(Domain.Entities.WebBudgetIncome webBudgetIncome);
+        Task CreateExpense(Domain.Entities.WebBudgetExpense webBudgetExpense);
 
 
-		// muszę mieć osobne Taski w interfejsie, aby pobrac z bazy danych pozniej w DBCOntextie
-		Task <IEnumerable<Domain.Entities.WebBudgetIncome>> GetAllIncomes();
+        // muszę mieć osobne Taski w interfejsie, aby pobrac z bazy danych pozniej w DBCOntextie
+        Task<IEnumerable<Domain.Entities.WebBudgetIncome>> GetAllIncomes();
 
-		Task <IEnumerable<Domain.Entities.WebBudgetExpense>> GetAllExpenses();
+        Task<IEnumerable<Domain.Entities.WebBudgetExpense>> GetAllExpenses();
 
-		Task<Domain.Entities.WebBudgetIncome> GetIncomeByEncodedName(string encodedIncomeName);
-		Task<Domain.Entities.WebBudgetExpense> GetExpenseByEncodedName(string encodedExpenseName);
+        Task<Domain.Entities.WebBudgetIncome> GetIncomeByEncodedName(string encodedIncomeName);
+        Task<Domain.Entities.WebBudgetExpense> GetExpenseByEncodedName(string encodedExpenseName);
 
-		Task<Domain.Entities.WebBudgetExpense> RemoveExpense(string endodedExpenseName);
+        Task<Domain.Entities.WebBudgetExpense> RemoveExpense(string endodedExpenseName);
 
-		Task<Domain.Entities.WebBudgetIncome> RemoveIncome(string encodedIncomeName);
+        Task<Domain.Entities.WebBudgetIncome> RemoveIncome(string encodedIncomeName);
 
-		Task CommitChanges();
+        Task CommitChanges();
 
-	}
+    }
 }
