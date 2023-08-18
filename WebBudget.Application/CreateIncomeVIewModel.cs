177be0a092a99bbce3fb2fe61b5b205a8c549c57
RdoG <radoslaw.gucwa.programista@gmail.com>
2023-08-18 22:24:53 +0200

    ok

---
+++ WebBudget.Application/CreateIncomeVIewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebBudget.Application.WebBudget.Commands.CreateWebBudgetIncome;
+
+namespace WebBudget.Application
+{
+	public class CreateIncomeVIewModel
+	{
+		public CreateWebBudgetIncomeCommand IncomeCommand { get; set; }
+		public List<IncomeCategory> IncomeCategories { get; set; }
+
+	}
+}
