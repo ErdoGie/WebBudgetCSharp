6c2943d1868524910a22655c4556bb9f281b5445
RdoG <radoslaw.gucwa.programista@gmail.com>
2023-09-04 11:36:46 +0200

    ExpenseId_migration_to_DB

---
+++ WebApplication.Infrastructure/Migrations/20230904093602_ExpenseId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebBudget.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExpenseId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EncodedExpenseName",
+                table: "WebBudgetExpense");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EncodedExpenseName",
+                table: "WebBudgetExpense",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
