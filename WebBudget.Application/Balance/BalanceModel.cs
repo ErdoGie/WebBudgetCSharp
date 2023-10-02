355f5a2dd0fe154bbc6278add8d04be973afac83
RdoG <radoslaw.gucwa.programista@gmail.com>
2023-10-02 12:05:16 +0200

    GeneratePdf_method_conditions_refactor

--- WebBudget.Application/Balance/BalanceModel.cs
+++ WebBudget.Application/Balance/BalanceModel.cs
@@ -44,81 +44,87 @@ namespace WebBudget.Application.Balance
 			document.Open();
 
 			bool hasIncomes = model.IncomeName?.Any() ?? false;
-
 			bool hasExpenses = model.ExpenseName?.Any() ?? false;
 
-			if (!hasIncomes || !hasExpenses)
+			if (!hasIncomes && !hasExpenses)
 			{
 				return new byte[0];
 			}
 
-
-			PdfPTable incomeTable = new PdfPTable(3);
-
-			PdfPCell incomeTitleCell = new PdfPCell(new Phrase("Incomes"));
-
-			incomeTitleCell.Colspan = 3;
-			incomeTitleCell.HorizontalAlignment = Element.ALIGN_CENTER;
-			incomeTable.AddCell(incomeTitleCell);
-
-			incomeTable.AddCell("Category");
-			incomeTable.AddCell("Value");
-			incomeTable.AddCell("Date");
-
-			for (int i = 0; i < model.IncomeName!.Count; i++)
+			if (hasIncomes)
 			{
-				incomeTable.AddCell(model.IncomeName[i]);
-				incomeTable.AddCell(model.IncomeValue![i].ToString("F2") + " zl");
-				var dateWithoutTime = model.IncomeDate![i].Date;
-				incomeTable.AddCell(dateWithoutTime.ToString("yyyy-MM-dd"));
+				PdfPTable incomeTable = new PdfPTable(3);
+
+				PdfPCell incomeTitleCell = new PdfPCell(new Phrase("Incomes"));
+
+				incomeTitleCell.Colspan = 3;
+				incomeTitleCell.HorizontalAlignment = Element.ALIGN_CENTER;
+				incomeTable.AddCell(incomeTitleCell);
+
+				incomeTable.AddCell("Category");
+				incomeTable.AddCell("Value");
+				incomeTable.AddCell("Date");
+
+				for (int i = 0; i < model.IncomeName!.Count; i++)
+				{
+					incomeTable.AddCell(model.IncomeName[i]);
+					incomeTable.AddCell(model.IncomeValue![i].ToString("F2") + " zl");
+					var dateWithoutTime = model.IncomeDate![i].Date;
+					incomeTable.AddCell(dateWithoutTime.ToString("yyyy-MM-dd"));
+				}
+
+				document.Add(incomeTable);
+				document.Add(new Paragraph(" "));
 			}
 
-			document.Add(incomeTable);
-			document.Add(new Paragraph(" "));
-
-
-			PdfPTable expenseTable = new PdfPTable(3);
-
-			PdfPCell expenseTitleCell = new PdfPCell(new Phrase("Expenses"));
-
-			expenseTitleCell.Colspan = 3;
-			expenseTitleCell.HorizontalAlignment = Element.ALIGN_CENTER;
-			expenseTable.AddCell(expenseTitleCell);
-
-			expenseTable.AddCell("Category");
-			expenseTable.AddCell("Value");
-			expenseTable.AddCell("Date");
-
-			for (int i = 0; i < model.ExpenseName!.Count; i++)
+			if (hasExpenses)
 			{
-				expenseTable.AddCell(model.ExpenseName[i]);
-				expenseTable.AddCell(model.ExpenseValue![i].ToString("F2") + " zl");
-				var dateWithoutTime = model.ExpenseDate![i].Date;
-				expenseTable.AddCell(dateWithoutTime.ToString("yyyy-MM-dd"));
+				PdfPTable expenseTable = new PdfPTable(3);
+
+				PdfPCell expenseTitleCell = new PdfPCell(new Phrase("Expenses"));
+
+				expenseTitleCell.Colspan = 3;
+				expenseTitleCell.HorizontalAlignment = Element.ALIGN_CENTER;
+				expenseTable.AddCell(expenseTitleCell);
+
+				expenseTable.AddCell("Category");
+				expenseTable.AddCell("Value");
+				expenseTable.AddCell("Date");
+
+				for (int i = 0; i < model.ExpenseName!.Count; i++)
+				{
+					expenseTable.AddCell(model.ExpenseName[i]);
+					expenseTable.AddCell(model.ExpenseValue![i].ToString("F2") + " zl");
+					var dateWithoutTime = model.ExpenseDate![i].Date;
+					expenseTable.AddCell(dateWithoutTime.ToString("yyyy-MM-dd"));
+				}
+
+				document.Add(expenseTable);
+				document.Add(new Paragraph(" "));
 			}
 
-			document.Add(expenseTable);
+			if (hasIncomes || hasExpenses)
+			{
+				PdfPTable totalsTable = new PdfPTable(3);
+				PdfPCell totalTitleCell = new PdfPCell(new Phrase("Summary"));
 
-			document.Add(new Paragraph(" "));
+				totalTitleCell.Colspan = 3;
+				totalTitleCell.HorizontalAlignment = Element.ALIGN_CENTER;
+				totalsTable.AddCell(totalTitleCell);
 
-			PdfPTable totalsTable = new PdfPTable(3);
-			PdfPCell totalTitleCell = new PdfPCell(new Phrase("Summary"));
+				totalsTable.AddCell("Total incomes: " + model.TotalIncome.ToString() + " zl");
+				totalsTable.AddCell("Total expenses: " + model.TotalExpense.ToString() + " zl");
+				totalsTable.AddCell("BALANCE: " + model.Balance.ToString() + " zl");
 
-			totalTitleCell.Colspan = 3;
-			totalTitleCell.HorizontalAlignment = Element.ALIGN_CENTER;
-			totalsTable.AddCell(totalTitleCell);
-
-			totalsTable.AddCell("Total incomes: " + model.TotalIncome.ToString() + " zl");
-			totalsTable.AddCell("Total expenses: " + model.TotalExpense.ToString() + " zl");
-			totalsTable.AddCell("BALANCE: " + model.Balance.ToString() + " zl");
-
-			document.Add(totalsTable);
+				document.Add(totalsTable);
+			}
 
 			document.Close();
 
 			byte[] pdfBytes = memoryStream.ToArray();
 			return pdfBytes;
 		}
+
 
 		public string GenerateCsv(BalanceModel model)
 		{
